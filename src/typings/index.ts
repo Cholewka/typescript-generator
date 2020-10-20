babdf62490f04739e40bfe7a4ebd507712516d66
Cholewka <mdcholewka@gmail.com>
2020-10-20 20:49:01 +0200

    Add new typings

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -3,12 +3,32 @@ export interface Settings {
 }
 
 export enum Preset {
-  UNDEFINED = 0,
-  RECOMMENDED = 1,
-  NODE10 = 2,
-  NODE12 = 3,
-  DENO = 4,
-  REACT_NATIVE = 5,
-  SVELTE = 6,
-  FROM_SCRATCH = 7,
+  UNDEFINED = "undefined",
+  RECOMMENDED = "recommended",
+  NODE10 = "node10",
+  NODE12 = "node12",
+  DENO = "deno",
+  REACT_NATIVE = "reactnative",
+  SVELTE = "svelte",
+  FROM_SCRATCH = "scratch",
+}
+
+export interface Question {
+  name: string;
+  description: string;
+  values: string[];
+  defaultValue: string;
+  presets: {
+    undefined: string;
+    recommended: string;
+    node10: string;
+    node12: string;
+    deno: string;
+    reactnative: string;
+    svelte: string;
+    scratch: string;
+  };
+
+  // optionals
+  linkToReference?: boolean;
 }
