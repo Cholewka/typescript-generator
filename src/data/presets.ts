86e31d3041ea09eed0bedd9a51cd96cc2e363d1a
Cholewka <mdcholewka@gmail.com>
2020-10-28 20:50:38 +0100

    data(db): Add new presets

---
+++ src/data/presets.ts
@@ -0,0 +1,11 @@
+import { Preset, Presets } from "../typings";
+
+const presets: Presets = [
+  {
+    name: "Recommended",
+    description: "hello",
+    presetEnum: Preset.RECOMMENDED,
+  },
+];
+
+export default presets;
