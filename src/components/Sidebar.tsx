210956d595cb881ba67d3488c9b96d4c28f68a30
Cholewka <mdcholewka@gmail.com>
2020-10-30 12:49:02 +0100

    feat(ctx): Implement steps in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 
+import { StepsContext } from "./App";
+
 import SidebarHeading from "./SidebarHeading";
 import SidebarParagraph from "./SidebarParagraph";
 
 import styles from "../styles/App.module.scss";
 
-const Sidebar = () => (
-  <section className={`${styles.App_gridItem} ${styles.App_sideItem}`}>
-    <SidebarHeading>Hello</SidebarHeading>
-    <SidebarParagraph>Hello 2</SidebarParagraph>
-  </section>
-);
+const Sidebar = () => {
+  const StepContext = React.useContext(StepsContext);
+  const currentStep = StepContext.getStep();
+
+  return (
+    <section className={`${styles.App_gridItem} ${styles.App_sideItem}`}>
+      <SidebarHeading>{currentStep.stepName}</SidebarHeading>
+      <SidebarParagraph>{currentStep.stepDescription}</SidebarParagraph>
+    </section>
+  );
+};
 
 export default Sidebar;
